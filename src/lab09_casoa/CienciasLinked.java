73f1861abceb12a24ea685aea69719c3ae85d791
Susana Bazan <subaez2810@gmail.com>
2026-06-06 12:09:58 -0500

    feat: Creacion de CienciasLinked que implementa CursosInterface

--- src/lab09_casoa/CienciasLinked.java
+++ src/lab09_casoa/CienciasLinked.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//BazanEspinoza
 package lab09_casoa;
 
 /**
  *
  * @author s
  */
-public class CienciasLinked {
-    
+public class CienciasLinked implements CursosInterface {
+    private String[] materias;
+
+    public CienciasLinked() {
+        materias = new String[]{"Fisica", "Quimica", "Biologia", "Matematica"};
+    }
+
+    @Override
+    public Iterator crearIterator() {
+        return new CursoIterator(materias);
+    }
 }
