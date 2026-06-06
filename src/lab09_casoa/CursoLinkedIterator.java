289a1863a56353c0a243f516c36ccb03a99e90cd
Susana Bazan <subaez2810@gmail.com>
2026-06-06 12:04:58 -0500

    feat: Creacion de CursoLinkedIterator con nodos enlazados

--- src/lab09_casoa/CursoLinkedIterator.java
+++ src/lab09_casoa/CursoLinkedIterator.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//BazanEspinoza
 package lab09_casoa;
 
 /**
  *
  * @author s
  */
-public class CursoLinkedIterator {
-    
+public class CursoLinkedIterator implements Iterator {
+    private static class Nodo {
+        String materia;
+        Nodo siguiente;
+        Nodo(String materia) { this.materia = materia; this.siguiente = null; }
+    }
+
+    private Nodo cabeza;
+    private Nodo actual;
+
+    public CursoLinkedIterator(String[] materias) {
+        cabeza = null;
+        for (int i = materias.length - 1; i >= 0; i--) {
+            Nodo n = new Nodo(materias[i]);
+            n.siguiente = cabeza;
+            cabeza = n;
+        }
+        actual = cabeza;
+    }
+    @Override
+    public void first() {
+        actual= cabeza;
+    }
+
+    @Override
+    public String next() {
+        String m = actual.materia;
+        actual = actual.siguiente;
+        return m;
+    }
+
+    @Override
+    public String currentItem() {
+        return actual !=null ? actual.materia : null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return actual != null;
+    }
 }
